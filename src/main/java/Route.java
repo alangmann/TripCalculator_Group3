c5d8db938e7535b2cd959365850abf1b82c066d9
koldaa10 <koldaa10@htlkaindorf.at>
2014-11-27 12:01:23 +0100

    Route finished

--- src/main/java/Route.java
+++ src/main/java/Route.java
@@ -3,5 +3,54 @@
  */
 public class Route
 {
+    // Beispiel: km;slope;routtype;special fee
 
+    private double distance;
+    private int slope;
+    private RouteType typeOfRoute;
+    private double specialFee;
+
+    public Route(double distance, int slope, RouteType typeOfRoute, double specialFee;) {
+        this.distance = distance;
+        this.slope = slope;
+        this.typeOfRoute = typeOfRoute;
+        this.specialFee = specialFee;
+    }
+
+    public enum RouteType {
+        HIGHWAY, COUNTRYROAD, GRAVELROAD
+    }
+
+    public int getSlope() {
+        return slope;
+    }
+
+    public void setSlope(int slope) {
+        this.slope = slope;
+    }
+
+    public RouteType getTypeOfRoute() {
+        return typeOfRoute;
+    }
+
+    public void setTypeOfRoute(RouteType typeOfRoute) {
+        this.typeOfRoute = typeOfRoute;
+    }
+
+    public double getSpecialFee() {
+        return specialFee;
+    }
+
+    public void setSpecialFee(double specialFee) {
+        this.specialFee = specialFee;
+    }
+
+    public double getDistance() {
+
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
--- src/main/java/TripCalculator.java
+++ src/main/java/TripCalculator.java
@@ -5,13 +5,15 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class TripCalculator {
 
     // TODO: implement me :)
     // test
-    private void readRoutes() throws Exception
+    private ArrayList<Route> readRoutes() throws Exception
     {
+        ArrayList<Route> routes = new ArrayList<Route>();
         String path = System.getProperty("Usr.src")+File.separator+"main"+File.separator+"resources";
         System.out.println(path);
         File f = new File(path);
@@ -20,8 +22,11 @@ public class TripCalculator {
         String line = "";
         while(!(line=br.readLine()).equals(null))
         {
+            Route r;
 
         }
+
+
     }
 
 
