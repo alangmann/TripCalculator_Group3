d103380e87a2e76193b8a23d6240f2f240414533
koldaa10 <koldaa10@htlkaindorf.at>
2014-11-27 11:42:15 +0100

    erste Methode

--- src/main/java/TripCalculator.java
+++ src/main/java/TripCalculator.java
@@ -1,11 +1,21 @@
 /**
  * Created by andreas.langmann on 12.11.2014.
  */
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class TripCalculator {
 
     // TODO: implement me :)
     // test
-
+    private void readRoutes() throws FileNotFoundException
+    {
+        String path = "";
+        File f = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(f));
+    }
 
 
 }
