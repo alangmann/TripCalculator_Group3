0a61d070348f00aa99af68e6bdf6803f06e096b6
koldaa10 <koldaa10@htlkaindorf.at>
2014-12-18 12:01:41 +0100

    GUI

--- src/main/java/TripCalculatorGUI.java
+++ src/main/java/TripCalculatorGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by Dominik on 04.12.2014.
@@ -13,14 +14,45 @@ public class TripCalculatorGUI {
     private JComboBox cbFuel;
     private JTextField tfCargo;
     private JTextField tfAxles;
+    private ButtonGroup vehicleGroup;
 
-    public void fillComboBoxes()
+
+
+    public void initialize()
     {
+        cbAdBlue.removeAllItems();
+        cbFuel.removeAllItems();
+        vehicleGroup = new ButtonGroup();
+        vehicleGroup.add(rbCar);
+        vehicleGroup.add(rbTruck);
+
         cbAdBlue.addItem("yes");
         cbAdBlue.addItem("no");
 
-        cbFuel.addItem("Diesel");
-        cbFuel.addItem("Patroleum");
+        cbFuel.addItem("DIESEL");
+        cbFuel.addItem("PATROL");
+
+        btAccept.addActionListener((java.awt.event.ActionListener) this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if(vehicleGroup.getSelection().equals(rbCar))
+        {
+            if(cbFuel.getSelectedItem().equals("DIESEL")) {
+                Car c = new Car(Integer.parseInt(tfCargo.getText()), Vehicle.fuelType.DIESEL, Double.parseDouble(tfConsumption.getText()));
+            }
+            else if(cbFuel.getSelectedItem().equals("PATROL"))
+            {
+                Car c = new Car(Integer.parseInt(tfCargo.getText()), Vehicle.fuelType.PATROL, Double.parseDouble(tfConsumption.getText()));
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Nicht alles ausgewählt");
+            }
+        }
+
+
     }
 
 }
