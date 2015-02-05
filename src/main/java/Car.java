f682113b23dc5b34a87e76a67127cc81a808fbef
rotdoa10 <rotdoa10@htlkaindorf.at>
2015-02-05 11:43:12 +0100

    Definitions hinzugefügt
    Dominik

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,10 @@
+import javax.annotation.Resource;
+
 /**
  * Created by Dominik on 27.11.2014.
  */
+
+@Repository("Car")
 public class Car extends Vehicle
 {
 
---
+++ src/main/java/definition/CarDAO.java
@@ -0,0 +1,7 @@
+package definition;
+
+/**
+ * Created by Dominik on 05.02.2015.
+ */
+public interface CarDAO {
+}
